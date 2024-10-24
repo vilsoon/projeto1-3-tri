d6f48f9acf55ab3341dd65fb1755f1acce587068
vilsoon <151634045+vilsoon@users.noreply.github.com>
2024-10-24 07:45:26 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+    cartao.className = "cartao"
 
-    cartao.innerHTML = `
-    <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
-    <div class="cartao__conteudo__pergunta">
-      
-    <p><h7>${pergunta}</h7></p> 
-    <p>  </p>
-    </div>
-    <div class="cartao__conteudo__resposta">
-       <p>              </p>
-    <p>${resposta}</p>
-    </div>
-    </div>
-    `
-    container.appendChild(cartao)
+cartao.innerHTML = `
+<div class="cartao__conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao__conteudo__pergunta">
+                       <p>${pergunta}</p> 
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>${resposta}</p> 
+                    </div>           
+                </div>
+                `
 
+    let respostaEstaVisivel = false 
+
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+    cartao.addEventListener('click', viraCartao)
+
+container.appendChild(cartao)
 }
--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,17 @@
 criaCartao(
-    'MINECRAFT',
-    'Por onde começo no minecraft?',
-    'Ao crias o mapa a melhor opção com certeza e pegar madeira'
+    'categoria',
+    'pergunta',
+    'resposta'
 )
 
 criaCartao(
-    'FREE FIRE',
-    'Qual melhor maneira de ficar bom no free fire?',
-    'jogamdo teinamento e aprendendo movimentação'
+    'categoria',
+    'pergunta',
+    'resposta'
 )
 
 criaCartao(
-    'BRAWN STARS',
-    'Como consigo mais brawlers?',
-    'A melhor maneira é cumprindo missões'
-)
-
-criaCartao(
-    'ROBLOX',
-    'Como consigo roupas de graça?',
-    'Acessando a loja ou com cofigos'
+    'categoria',
+    'pergunta',
+    'resposta'
 )
